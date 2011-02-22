0fc20304f102046b7871ad5b2d7e520aac5631fe
moechofe <service@moechofe.com>
2011-02-22 17:39:45 +0100

    add: simple tests

--- test.php
+++ test.php
@@ -3,4 +3,26 @@
 require_once 'zimple-test.php';
 require_once 'kyoto-tycoon.php';
 
-is( kt\get( 'japan',
+test(
+
+	'Test simple operation: get,set,clear,replace,add,append', function()
+	{
+		$kt = kt('http://martibox:1978');
+		ok( $kt->clear );
+		isnull( $kt->replace('a','academy') );
+		isnull( $kt->get('a') );
+		ok( $kt->add('a', 'alien') );
+		is( $kt->get('a'), 'alien' );
+		ok( $kt->set('a', 'ananas') );
+		is( $kt->get('a'), 'ananas' );
+		ok( $kt->replace('a', 'akira') );
+		is( $kt->get('a'), 'akira');
+		isnull( $kt->add('a', 'aligator') );
+		is( $kt->get('a'), 'akira' );
+		ok( $kt->append('a'), ' kurozawa' );
+		is( $kt->get('a'), 'akira kurozawa' );
+	}
+
+);
+
+
